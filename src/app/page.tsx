36b7bbd0de6ec3fc8f51aaf3041d7a363b4add25
sharekim <sksyag@naver.com>
2025-04-03 11:54:12 +0900

    Revert "fix: unset canvas origin width,height"
    This reverts commit bac8cf26afbdb499295df4e68974021f5f93ced3.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,37 @@
 "use client";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
 
-// CanvasArea를 별도로 로드
 const CanvasArea = dynamic(() => import("@/components/molecules/CanvasArea"), {
   ssr: false,
 });
 
+const IntroComponent = dynamic(() => import("@/components/Intro"), {
+  ssr: false,
+});
+const SkillsComponent = dynamic(() => import("@/components/Skills"), {
+  ssr: false,
+});
+const ExperienceComponent = dynamic(() => import("@/components/Experience"), {
+  ssr: false,
+});
+const ProjectsComponent = dynamic(() => import("@/components/Projects"), {
+  ssr: false,
+});
+const FloatingActionButtonComponent = dynamic(
+  () => import("@/components/molecules/FloatingActionButton"),
+  { ssr: false }
+);
+
 export default function Home() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  const IntroComponent = dynamic(() => import("@/components/Intro"), {
-    ssr: false,
-  });
-  const SkillsComponent = dynamic(() => import("@/components/Skills"), {
-    ssr: false,
-  });
-  const ExperienceComponent = dynamic(() => import("@/components/Experience"), {
-    ssr: false,
-  });
-  const ProjectsComponent = dynamic(() => import("@/components/Projects"), {
-    ssr: false,
-  });
-  const FloatingActionButtonComponent = dynamic(
-    () => import("@/components/molecules/FloatingActionButton"),
-    { ssr: false }
-  );
-
   return (
     <>
-      {mounted && <CanvasArea />}
       <div className="container w-screen h-full px-8 md:px-20">
         <IntroComponent />
         <SkillsComponent />
         <ExperienceComponent />
         <ProjectsComponent />
         <FloatingActionButtonComponent />
+        <CanvasArea />
       </div>
     </>
   );
