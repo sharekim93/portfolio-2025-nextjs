40184fdba3b97c28b022def2b09ac5f489bd19ce
sharekim <sksyag@naver.com>
2025-04-16 14:32:08 +0900

    feature: resume PDF 미리보기 및 다운로드 (WIP)

--- src/app/components/resume/Header.tsx
+++ src/app/components/resume/Header.tsx
@@ -1,5 +1,7 @@
 import { BACKGROUND_COLOR } from "@/constants";
-import { Printer } from "lucide-react";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import { Download, Printer } from "lucide-react";
 import React from "react";
 import { useReactToPrint } from "react-to-print";
 
@@ -13,12 +15,42 @@ export const Header = ({ ref }: { ref?: React.Ref<HTMLDivElement> }) => {
     contentRef: ref as React.RefObject<HTMLDivElement>,
   });
 
+  const handleDownloadPdf = async () => {
+    const elementRef = ref as React.RefObject<HTMLDivElement> | null;
+    const element = elementRef?.current;
+    if (!element) {
+      return;
+    }
+    const canvas = await html2canvas(element);
+    const componentWidth = element.offsetWidth;
+    const componentHeight = element.offsetHeight;
+
+    const orientation = componentWidth >= componentHeight ? "l" : "p";
+
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF({
+      orientation,
+      unit: "px",
+    });
+
+    pdf.internal.pageSize.width = componentWidth;
+    pdf.internal.pageSize.height = componentHeight;
+
+    pdf.addImage(imgData, "PNG", 0, 0, componentWidth, componentHeight);
+    pdf.save("resume_김나눔.pdf");
+  };
+
   return (
     <>
       <header className="mb-4 sm:mb-8 flex justify-between items-end">
         <h1 className="text-lg sm:text-3xl font-bold">
           {data.name}, {data.title}
         </h1>
+        <Download
+          className="print:hidden"
+          stroke={BACKGROUND_COLOR}
+          onClick={() => handleDownloadPdf()}
+        />
         <Printer
           className="print:hidden"
           stroke={BACKGROUND_COLOR}
