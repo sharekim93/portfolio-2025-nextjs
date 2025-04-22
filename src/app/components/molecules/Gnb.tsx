11f807389d7bdd51faf9a4cc0fec5aae1b648963
sharekim93 <sksyag@naver.com>
2025-04-22 16:32:10 +0900

    refactor: Gnb z-index 추가

--- src/app/components/molecules/Gnb.tsx
+++ src/app/components/molecules/Gnb.tsx
@@ -16,7 +16,7 @@ const GnbMenu = () => {
 
   return (
     <div
-      className={`flex items-center justify-start pl-3 pb-3 fixed top-0 left-0 right-0 bg-[#FAF9F6] border-b-[0.5px] ${
+      className={`flex items-center justify-start pl-3 pb-3 fixed top-0 left-0 right-0 z-1 bg-[#FAF9F6] border-b-[0.5px] ${
         activeTabIndex === 1 && "bg-white"
       }`}
     >
