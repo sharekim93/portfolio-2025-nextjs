4290bc9152d0587d53a12c98311a3d0a32d7e94d
sharekim93 <sksyag@naver.com>
2025-04-23 14:04:27 +0900

    refactor: Introduce - Skills Section 스킬별 Icon 부여 및 PopOver 적용(WIP)

--- src/app/components/introduce/Skills.tsx
+++ src/app/components/introduce/Skills.tsx
@@ -5,6 +5,13 @@ import { fadeIn } from "@/styles/animation";
 import useObserver from "@/hooks/useObserver";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
 import { Badge } from "@/components/ui/Badge";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/PopOver";
+import { RiCss3Fill, RiHtml5Fill } from "react-icons/ri";
+import { isMobile } from "react-device-detect";
 
 export default function Skills() {
   const { ref, animation } = useObserver();
@@ -20,14 +27,46 @@ export default function Skills() {
       className=" w-full h-full min-h-screen flex flex-col justify-start items-start mt-10"
     >
       <h2 className="text-3xl font-bold mb-4 text-foreground ">Skills</h2>
+      <p className="pb-4 text-gray-600 font-light text-sm sm:text-base">
+        버튼을 {isMobile ? "터치" : "클릭"}해보세요
+      </p>
       <div className="w-full flex flex-wrap gap-2 sm:flex">
         <Card className="flex-1/2 sm:flex-1">
           <CardHeader>
             <CardTitle>Core</CardTitle>
           </CardHeader>
           <CardContent className="flex flex-wrap gap-2">
-            <Badge variant="outline">HTML</Badge>
-            <Badge variant="outline">CSS</Badge>
+            <Badge
+              variant="outline"
+              className="flex justify-center items-center p-2 w-20"
+              title="HTML"
+            >
+              <Popover>
+                <PopoverTrigger className="flex justify-center items-center">
+                  <RiHtml5Fill size={30} color="#E44D26" /> HTML
+                </PopoverTrigger>
+                <PopoverContent>
+                  마크업 언어를 활용해 웹 페이지를 구조화 할 수 있습니다. 시멘틱
+                  태그 활용 및 웹 접근성에 관심이 많습니다.
+                </PopoverContent>
+              </Popover>
+            </Badge>
+            <Badge
+              variant="outline"
+              className="flex justify-center items-center p-2 w-20"
+              title="CSS"
+            >
+              <Popover>
+                <PopoverTrigger className="flex justify-center items-center">
+                  <RiCss3Fill size={30} color="#264de4" /> CSS
+                </PopoverTrigger>
+                <PopoverContent>
+                  CSS 선택자를 활용한 스타일링에 능숙합니다. @규칙(At rules)를
+                  활용한 적응형 디자인과 grid, flex 를 활용한 반응형 디자인,
+                  애니메이션 효과 등을 부여할 수 있습니다.
+                </PopoverContent>
+              </Popover>
+            </Badge>
             <Badge variant="outline">Javascript</Badge>
             <Badge variant="outline">Typescript</Badge>
             <Badge variant="outline">Java</Badge>
