fe6d062f2d2753056af0710cedcbb98e0dee805b
sharekim93 <sksyag@naver.com>
2025-04-24 15:34:43 +0900

    refactor: Skills section 을 Card 형식에서 문서 형태로 변경(WIP)

--- src/app/components/introduce/Skills.tsx
+++ src/app/components/introduce/Skills.tsx
@@ -5,13 +5,12 @@ import { fadeIn } from "@/styles/animation";
 import useObserver from "@/hooks/useObserver";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
 import { Badge } from "@/components/ui/Badge";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/PopOver";
-import { RiCss3Fill, RiHtml5Fill } from "react-icons/ri";
-import { isMobile } from "react-device-detect";
+
+import htmlLogo from "@/images/HTML5_Badge.svg";
+import cssLogo from "@/images/Official_CSS_Logo.svg";
+import javascriptLogo from "@/images/Unofficial_JavaScript_logo_2.svg";
+import typescriptLogo from "@/images/ts-logo-128.svg";
+import Image from "next/image";
 
 export default function Skills() {
   const { ref, animation } = useObserver();
@@ -26,56 +25,81 @@ export default function Skills() {
       variants={fadeIn}
       className=" w-full h-full min-h-screen flex flex-col justify-start items-start mt-10"
     >
-      <h2 className="text-3xl font-bold mb-4 text-foreground ">Skills(WIP)</h2>
-      <p className="pb-4 text-gray-600 font-light text-sm sm:text-base">
-        버튼을 {isMobile ? "터치" : "클릭"}해보세요
-      </p>
+      <h2 className="text-3xl font-bold mb-4 text-foreground ">Skills</h2>
       <div className="w-full flex flex-wrap gap-2 sm:flex">
-        <Card className="flex-1/2 sm:flex-1">
-          <CardHeader>
-            <CardTitle>Core</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-wrap gap-2">
-            <Badge
-              variant="outline"
-              className="flex justify-start items-center p-2 w-20"
-              title="HTML"
-            >
-              <Popover>
-                <PopoverTrigger className="flex justify-start items-center">
-                  <RiHtml5Fill size={30} color="#E44D26" />
-                  <span>HTML</span>
-                </PopoverTrigger>
-                <PopoverContent>
-                  마크업 언어를 활용해 웹 페이지를 구조화 할 수 있습니다. 시멘틱
-                  태그 활용 및 웹 접근성에 관심이 많습니다.
-                </PopoverContent>
-              </Popover>
-            </Badge>
-            <Badge
-              variant="outline"
-              className="flex justify-start items-center p-2 w-20"
-              title="CSS"
-            >
-              <Popover>
-                <PopoverTrigger className="flex justify-start items-center">
-                  <RiCss3Fill size={30} color="#264de4" />
-                  <span>CSS</span>
-                </PopoverTrigger>
-                <PopoverContent>
-                  CSS 선택자를 활용한 스타일링에 능숙합니다. @규칙(At rules)를
-                  활용한 적응형 디자인과 grid, flex 를 활용한 반응형 디자인,
-                  애니메이션 효과 등을 부여할 수 있습니다.
-                </PopoverContent>
-              </Popover>
-            </Badge>
-            <Badge variant="outline">Javascript</Badge>
-            <Badge variant="outline">Typescript</Badge>
-            <Badge variant="outline">Java</Badge>
-            <Badge variant="outline">Python</Badge>
-            <Badge variant="outline">C++</Badge>
-          </CardContent>
-        </Card>
+        <h3 className="text-pink-500 text-xl font-semibold">Core</h3>
+        <div className="h-0 w-full border-b-1" />
+        <ul title="Core" className="flex flex-col gap-4 pl-1">
+          <li>
+            <div className="flex flex-wrap gap-1">
+              <Image
+                src={htmlLogo}
+                alt="html Logo"
+                width={22}
+                height={22}
+                style={{ borderRadius: "10%" }}
+              />
+              <span className="font-semibold">HTML</span>
+            </div>
+            <p className="text-xs sm:text-sm pl-6.5">
+              마크업 언어를 활용해 웹 페이지를 구조화 할 수 있습니다. 시멘틱
+              태그 활용 및 웹 접근성에 관심이 많습니다.
+            </p>
+          </li>
+          <li>
+            <div className="flex flex-wrap gap-1">
+              <Image
+                src={cssLogo}
+                alt="css Logo"
+                width={22}
+                height={22}
+                style={{ borderRadius: "10%" }}
+              />
+              <span className="font-semibold">CSS</span>
+            </div>
+            <p className="text-xs sm:text-sm pl-6.5">
+              CSS 선택자를 활용한 스타일링에 능숙합니다. @규칙(At rules)를
+              활용한 적응형 디자인과 grid, flex 를 활용한 반응형 디자인,
+              애니메이션 효과 등을 부여할 수 있습니다.
+            </p>
+          </li>
+          <li>
+            <div className="flex flex-wrap gap-1">
+              <Image
+                src={javascriptLogo}
+                alt="javascript Logo"
+                width={20}
+                height={20}
+                style={{ borderRadius: "10%" }}
+              />
+              <span className="font-semibold">Javascript</span>
+            </div>
+            <p className="text-xs sm:text-sm pl-6.5">
+              ES6 이후의 모던 자바스크립트 문법에 관심이 있습니다. spread 문법
+              등을 활용한 코드 가독성 및 성능 향상에 관심이 있습니다.
+            </p>
+          </li>
+          <li>
+            <div className="flex flex-wrap gap-1">
+              <Image
+                src={typescriptLogo}
+                alt="typescript Logo"
+                width={20}
+                height={20}
+                style={{ borderRadius: "10%" }}
+              />
+              <span className="font-semibold">Typescript</span>
+            </div>
+            <p className="text-xs sm:text-sm pl-6.5">
+              변수 및 상수에 대한 타입 지정, Class, Utility 등 컴파일 시
+              javascript 의 안정성을 더해주는 타입스크립트 활용에 익숙합니다
+            </p>
+          </li>
+        </ul>
+        <Badge variant="outline">Java</Badge>
+        <Badge variant="outline">Python</Badge>
+        <Badge variant="outline">C++</Badge>
+
         <Card className="flex-1/2 sm:flex-1">
           <CardHeader>
             <CardTitle>Frontend</CardTitle>
