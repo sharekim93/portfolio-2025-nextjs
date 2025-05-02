c4d2f1b164fd5aab630a6b463442e21094835a14
sharekim93 <sksyag@naver.com>
2025-05-02 15:36:44 +0900

    refactor: canvasArea 걷어내기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 "use client";
-import dynamic from "next/dynamic";
 import { useMenuStore } from "@/store/menuStore";
 import GnbMenu from "@/components/molecules/Gnb";
 import Resume from "@/components/resume";
 import Introduce from "@/components/introduce";
-
-const CanvasArea = dynamic(() => import("@/components/molecules/CanvasArea"), {
-  ssr: false,
-});
 
 export default function Home() {
   const tabIndex = useMenuStore((state) => state.tabIndex);
@@ -15,7 +10,7 @@ export default function Home() {
     <div className="w-full h-full ">
       <GnbMenu />
       <div className="mt-10">{tabIndex === 0 ? <Introduce /> : <Resume />}</div>
-      {tabIndex === 0 ? <CanvasArea /> : undefined}
+      {/* {tabIndex === 0 ? <CanvasArea /> : undefined} */}
     </div>
   );
 }
